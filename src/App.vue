b0051c1f7235d1d5e549105aa465d42754d45e9d
kt474 <kt474@cornell.edu>
2026-01-10 10:28:56 -0500

    Add vercel analytics

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import Main from "./components/Main.vue";
+import { Analytics } from "@vercel/analytics/vue";
 </script>
 
 <template>
+  <Analytics />
   <Main />
 </template>
 
