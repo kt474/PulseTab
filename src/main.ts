ef37980c03802aef315e002a548d4ae783aa8f8c
kt474 <kt474@cornell.edu>
2026-01-14 16:36:55 -0500

    Remove vercel analytics

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,7 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
-import { inject } from "@vercel/analytics";
 
 const app = createApp(App);
-
-inject();
 
 app.mount("#app");
